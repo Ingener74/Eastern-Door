05b1dd415c42e2dbbadd2dee72744d2386774a9d
Shnaider Pavel <shnaiderpasha@gmail.com>
2016-10-16 00:13:20 +0300

    компонент Сеть для работы с сокетами

---
+++ src/app/controllers/Net.lua
@@ -0,0 +1,43 @@
+local socket = require("socket")
+
+local Net = class("Net")
+
+function Net:ctor(host, port)
+	self.sock = assert(socket.connect(host, port))
+end
+
+function Net:stop()
+	self.sock:close()
+end
+
+function Net.text(str)
+	return str
+end
+
+function Net.json(str)
+	return json.decode(str)
+end
+
+function Net:request(request, method)
+	return Promise.new(function(resolve, reject)
+		local method_ = method or Net.json
+		self.sock:send(request)
+		self.sock:settimeout(0)
+	    self.schedulerEntry = cc.Director:getInstance():getScheduler():scheduleScriptFunc(function()
+			local status, msg = xpcall(function()
+				local chunk, status, partial = self.sock:receive(1024)
+				if partial ~= "" then
+					cc.Director:getInstance():getScheduler():unscheduleScriptEntry(self.schedulerEntry)
+					resolve(method_(partial))
+				end
+			end, __G__TRACKBACK__)
+			if not status then
+			    print(msg)
+			    reject(msg)
+			end
+
+	    end, 0, false)
+	end)
+end
+
+return Net
--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -1,4 +1,6 @@
 
+local Net = require("app.controllers.Net")
+
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 function MainScene:onCreate()
@@ -78,6 +80,37 @@ function MainScene:onCreate()
 		end
 	end))
 
+	table.insert(buttons, self:addButton("Create net", function()
+		self._net = Net:create('127.0.0.1', 9999)
+	end))
+
+	table.insert(buttons, self:addButton("Net request m1, m2", function()
+		self._net:request("{\"method\":\"m1\"}"):andThen(function(result)
+			dump(result)
+			return self._net:request("{\"method\":\"m2\"}")
+		end):andThen(function(result)
+			dump(result)
+		end):catch(function(err)
+			printError(err)
+		end)
+	end))
+
+	table.insert(buttons, self:addButton("Net request m3, m4", function()
+		self._net:request("{\"method\":\"m3\"}"):andThen(function(result)
+			dump(result)
+			return self._net:request("{\"method\":\"m4\"}")
+		end):andThen(function(result)
+			dump(result)
+		end):catch(function(err)
+			printError(err)
+		end)
+	end))
+
+	table.insert(buttons, self:addButton("Remove net", function()
+		self._net:request("quit")
+		self._net:stop()
+	end))
+
 	local x = 8
 	local y = display.height - 8
 
@@ -85,6 +118,10 @@ function MainScene:onCreate()
 		button:setPosition(x, y)
 
 		x = x + button:getContentSize().width + 8
+		if (x + 120 + 8) >= display.width then
+			x = 8
+			y = y - 40 - 8
+		end
 	end
 end
 
@@ -94,6 +131,7 @@ function MainScene:addButton(title, handler)
 	button:setScale9Enabled(true)
 	button:setContentSize(120, 40)
 	button:setTitleText(title)
+	button:setTitleFontSize(12)
 	button:addClickEventListener(handler)
 	self:addChild(button)
 	return button
